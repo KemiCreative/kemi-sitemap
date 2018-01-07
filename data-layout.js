437986730b1f63486d778e05fb5e7c738b24890e
Michael Home <michael@Michaels-iMac.local>
2018-01-06 19:01:59 -0500

    Working on hooking up javascript.  When you change label it is change in the viewing window as well

--- data-layout.js
+++ data-layout.js
@@ -1,4 +1,4 @@
-/**/
+/*
 const $value = [
 	posts = {
 		active: true,
@@ -26,56 +26,57 @@ const $value = [
 		excludes: '1,2,12,145',
 	},
 ];
-/**/
+*/
 
 (function($){
   $(document).ready(() => {
 
-  	// const cpts = document.querySelectorAll('.kemiSitemap-cpt');
-  	// const checkbox = cpts.querySelector('input[type=checkbox]');
-		// const label = cpts.querySelector('.kemiSitemap-cpt-label');
-  	const input = document.querySelector('#testing');
+		const kemi_sitemap = document.querySelector('.kemisitemap_cpts');
+		const cpt_blocks = document.querySelectorAll('.kemi-sitemap-pt-block');
 
-		input.value = JSON.stringify($value);
-
-  	console.log($value);
-
-
-		// ********* let labelPrev = '';
-
-
-  	// ********* checkbox.addEventListener('click', toggleCPT);
-		// ********* label.addEventListener('focus', labelFocus);
-		// ********* label.addEventListener('change', labelChange);
-
+		let labelPrev = '';
 
 
 		/*
 		 * Description: Function to Toggle the CPT in Viewing Window
 		 */
-		// ********* const toggleCPT = (event) => {
-			// ********* if(event.target.checked){
-				// Show the CPT in the Viewing Window
-			// ********* } else {
-				// Remove the CPT from the Viewing Window
-			// ********* }
-		// ********* }
+		const toggleCPT = (event) => {
+			if(event.target.parentNode.className == 'switch'){
+				if(event.target.checked){
+					// Show the CPT in the Viewing Window
+					console.log('checked');
+				} else {
+					// Remove the CPT from the Viewing Window
+					console.log('unchecked');
+				}
+			}
+		}
 
 		/*
 		 * Description: Function to set the current label value before change
 		 */
-		// ********* const labelFocus = (event) => {
-			// ********* labelPrev = event.target.value;
-		// ********* }
+		const labelFocus = (event) => {
+			labelPrev = event.target.value;
+		}
 
 		/*
 		 * Description: Function to change the CPT label in the Viewing Window
 		 */
-		// *********** const labelChange = (event) => {
+		const labelChange = (event) => {
 			// Find the VIEWING WINDOW CPT based on labelPrev variable
-			// *********** 'VIEWING WINDOW CPT' = event.target.value;
+			// 'VIEWING WINDOW CPT' = event.target.value;
+			for(let i = 0; i < cpt_blocks.length; i++){
+				let block = cpt_blocks[i].querySelector('h3');
+				if(block.textContent == labelPrev){
+					block.textContent = event.target.value;
+				}
+			}
 
-		// *********** }
+		}
+
+		$('.kemisitemap_cpts .switch input').on('click', toggleCPT);
+		$('.kemisitemap-cpt-label').on('focus', labelFocus);
+		$('.kemisitemap-cpt-label').on('change', labelChange);
 
   	/*
   	let output = [];
@@ -92,15 +93,8 @@ const $value = [
 
   		value.includes.taxonomies = checkbox.getAttribute('taxonomy');
 
-  		output.push(value);*/
-  	}
-
-  	console.log(output);
-
-  	input.value = JSON.stringify($value);
-
-  	console.log($value);
-  	*/
+  		output.push(value);
+  	}*/
 
   });
 })(jQuery);
